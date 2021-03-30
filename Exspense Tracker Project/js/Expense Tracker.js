a85346338bbe8592c18193ee9cce6247c4e19c5b
KCSmall <kirklandsmall@gmail.com>
2021-03-29 21:52:50 -0400

    Update Expense Tracker.js

--- Exspense Tracker Project/js/Expense Tracker.js
+++ Exspense Tracker Project/js/Expense Tracker.js
@@ -1,5 +1,9 @@
 //*calling Javascript id names 
-var addToDoButton ,
-var toDoContainer ,
-var inputField ,
+Var Expense = {
+    name: '',
+    date:'' , + , 
+    amount : //* that same function can be written similarly to this function to add the amount
+    //* the end result of this Javascript page should end up looking like a repeated script
 
+
+    
