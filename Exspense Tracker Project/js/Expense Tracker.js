5a6e268f166f10e73ce90f62a4be0555e637a216
KCSmall <kirklandsmall@gmail.com>
2021-03-30 16:47:58 -0400

    Update Expense Tracker.js

--- Exspense Tracker Project/js/Expense Tracker.js
+++ Exspense Tracker Project/js/Expense Tracker.js
@@ -1,9 +1,12 @@
-//*calling Javascript id names 
-Var Expense = {
-    name: '',
-    date:'' , + , 
-    amount : //* that same function can be written similarly to this function to add the amount
-    //* the end result of this Javascript page should end up looking like a repeated script
+//*calling Javascript id names . The interperter always looks for functions first before global scopes
+    function () {
+        var name
+        var date
+        var amount
+        return name + date + amount;
+    };
 
+//* that same function can be written similarly to this function to add the amount
+//* the end result of this Javascript page should end up looking like a repeated script
 
     
